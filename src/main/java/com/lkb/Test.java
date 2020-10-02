f26e373485c4ebf4e3b543e3ceeb27320ec0f412
lalitbehera <lalitkumar.b@landmarkgroup.in>
2020-10-02 10:53:40 +0530

    first commit

---
+++ src/main/java/com/lkb/Test.java
@@ -0,0 +1,7 @@
+package com.lkb;
+
+public class Test {
+    public static void helloFromJava(){
+        System.out.println("Hello there I am Java");
+    }
+}
---
+++ src/main/kotlin/com/lkb/main.kt
@@ -0,0 +1,8 @@
+package com.lkb
+
+import com.lkb.Test.helloFromJava
+
+fun main() {
+    println("Hello from kotlin")
+    helloFromJava()
+}
